9b4d9e5b4586d6fc6f6f7e2b7c4b389c2ceafaf4
patilna <patilna@FCP21029-D>
2018-03-27 11:14:57 +0530

    commiting

---
+++ VideoSplashXammie/VideoSplashXammie/VideoSplashActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+
+namespace VideoSplashXammie
+{
+    [Activity(Label = "VideoSplashActivity")]
+    public class VideoSplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            Task t1
+
+        }
+    }
+}
